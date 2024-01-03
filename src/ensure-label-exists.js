c3b5ce85379e8d43340b521ccf8acdbf2c16356a
lmnleaf <lmnleaf@users.noreply.github.com>
2024-01-03 14:55:34 -0700

    list and create labels

---
+++ src/ensure-label-exists.js
@@ -0,0 +1,26 @@
+async function ensureLabelExists(owner, repo, labelName, description, color, octokit) {
+  try {
+    let labels = await octokit.rest.issues.listLabelsForRepo({
+      owner: owner,
+      repo: repo,
+    });
+  
+    let exists = labels.data.some(label => label.name === labelName);
+  
+    if (!exists) {
+      await octokit.rest.issues.createLabel({
+        owner: owner,
+        repo: repo,
+        name: labelName,
+        description: description,
+        color: color
+      });
+
+      console.log('Added label: ', labelName);
+    }
+  } catch(error) {
+    throw error;
+  }
+}
+
+module.exports = ensureLabelExists
