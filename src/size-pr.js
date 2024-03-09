a767651b30337d4bd902080ac751122cfb9213f8
lmnleaf <lmnleaf@users.noreply.github.com>
2024-03-09 12:14:37 -0700

    fixes typo

--- src/size-pr.js
+++ src/size-pr.js
@@ -6,7 +6,7 @@ const labelPR = require('./label-pr.js');
 async function sizePR(specsInput, directoriesInput, color, context, octokit) {
   try {
     console.log('Excluded Directories', excludedDirectories);
-    console.log('Excluded Specs', excludedSpecs);
+    console.log('Excluded Specs', excludeSpecs);
     const repo = context.repo;
     const prNumber = context.payload.pull_request.number;
     const prLabels = context.payload.pull_request.labels;
